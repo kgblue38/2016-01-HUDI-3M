258fe55fd081e0d61fb849d7d96574acba9bcf24
Chiho <gm@haeyum.org>
2016-03-26 14:22:05 +0900

    gameMain

---
+++ src/main/webapp/gameMain.jsp
@@ -0,0 +1,92 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+      <style type="text/css">      
+        </style>
+        <link rel="stylesheet" type="text/css" href="css/gameMain.css">
+        <script type="text/javascript"
+			src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript"
+			src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+		<script type="text/javascript"></script>
+        <script src="lib/jquery-1.12.0.js"></script>
+        <script src="lib/handlebars-v4.0.5.js"></script>
+        <link rel="stylesheet" href="css/remodal.css">
+        <link rel="stylesheet" href="css/remodal-default-theme.css">
+        
+    </head>
+    <body>
+         <div class="header">
+            <div class="playerA">
+                 <div class="playerAAvatar"><img src="image/avartar_in_circle.png"></div>
+                 <div class="playerAInformation">Id: Junnie </br> 계급: 루비</div>
+                 <div class="playerAScore">53(점)</div>
+            </div>
+               
+            <div class="playerB">
+                <div class="playerBAvatar"><img src="image/avartar2_in_circle.png"></div>
+                <div class="playerBInformation">Id: Chiho </br> 계급: 사파이어</div>
+                <div class="playerBScore"> 점수
+                </div>
+            </div>
+       </div>
+
+       <div class="header_line"></div>
+       <div class="middleArea">
+        <div class="gameArea">
+            <div class="leftUserArea">
+                <div class="leftUser"> 
+                	<img src="image/leftUser_card.png">
+                </div>
+            </div>
+             <div class="matchArea">
+                    <div class="match">
+                    	<div class="leftUser_card">
+                        <!-- <img src="image/card/back.png"> -->
+                        </div>
+
+                    	<div class="rightUser_card">
+                        <!-- <img src="image/card/back.png"> -->
+                    	</div>
+                    </div>
+            </div> 
+            <div class="rightUserArea">
+                <div class="rightUser">
+                	<img src="image/rightUser_card.png">
+                </div>
+            </div>
+            <div class="resultEvent">
+
+            </div>
+          </div>
+       </div>
+       <div class="clear"></div>
+       <div class="botton_container">
+         
+        <a id="result" href="#modal">See the Result.</a>
+                 
+        <div class="remodal" data-remodal-id="modal">
+          <button data-remodal-action="close" class="remodal-close"></button>
+          <h1></h1>
+          <p>
+            
+          </p>
+          <br>
+          <button data-remodal-action="confirm" class="remodal-confirm">OK</button>
+          <button data-remodal-action="cancel" class="remodal-cancel">Get Back to Waiting Room</button>
+        </div>
+                
+       </div>
+       <div class="footer">
+            <div class="footerCard">
+            </div>
+       </div>
+       <script src="js/gameMain.js"></script>
+       <script src="js/remodal.js"></script>
+
+    </body>
+</html>
---
+++ src/main/webapp/js/gameMain.js
@@ -0,0 +1,111 @@
+var CARD = (function(){
+	var cardTemplate = "<img src='image/card/{{random_card}}h.png'>";
+	var cardTemplate_1 = "<img src='image/card/{{random_card}}s.png'>";
+	var template_up = Handlebars.compile(cardTemplate);
+	var template_up_1 = Handlebars.compile(cardTemplate_1);
+	var resultTemplate = "<p>PlayerA got {{card_NumberA}} Space<br>"+
+						"PlayerB got {{card_NumberB}} Heart</p>";
+	var resultHeaderTemplate = "<h1>Player{{winner}}</h1>";
+	var template_up_result = Handlebars.compile(resultTemplate);
+	var template_up_resultHeader = Handlebars.compile(resultHeaderTemplate);
+
+	function getCard(){
+
+	}
+
+	function revealCard(){
+		var random_card = Math.floor((Math.random() * 10) + 1);
+		var random_card_1 = Math.floor((Math.random() * 10) + 1);
+		// $(".leftUser_card").append($(template_up({"random_card":random_card})));
+		$(".leftUser_card").delay(1000).queue(function(){
+			$(this).css('background-image', 'url("image/card/'+random_card+'s.png")');
+		});
+		// $(".rightUser_card").append($(template_up_1({"random_card":random_card_1})));
+		$(".rightUser_card").delay(1000).queue(function(){
+			$(this).css('background-image', 'url("image/card/'+random_card_1+'h.png")');
+		});
+		if(random_card>random_card_1){
+				var result = "A wins.";
+				$('#result').remove();
+				$('.playerA').delay(1200).queue(function(){
+					$(this).css('border', '5px solid red');
+				});
+				$(".remodal p").append($(template_up_result({"card_NumberA":random_card, 
+					"card_NumberB":random_card_1})));
+				$(".remodal h1").append($(template_up_resultHeader({"winner":result})));
+		}else if (random_card == random_card_1){
+				var result = "s draws.";
+				$('#result').remove();
+				$(".remodal p").append($(template_up_result({"card_NumberA":random_card, 
+					"card_NumberB":random_card_1})));
+				$(".remodal h1").append($(template_up_resultHeader({"winner":result})));
+		}else{
+				var result = "B wins."
+				$('#result').remove();
+				$('.playerB').delay(1200).queue(function(){
+					$(this).css('border', '5px solid red');
+				});
+				$(".remodal p").append($(template_up_result({"card_NumberA":random_card, 
+					"card_NumberB":random_card_1})));
+				$(".remodal h1").append($(template_up_resultHeader({"winner":result})));
+		}
+	}
+	
+	
+
+	function init(){
+		 $('#result').on('click', revealCard);
+		// $('.gameArea').on('click', '.righttUser_card', getCardRight);
+		
+	}
+
+	// var cardArray = [
+	// 	{"type": "h", "number": "1"},
+	// 	{"type": "h", "number": "2"},
+	// 	{"type": "h", "number": "3"},
+	// 	{"type": "h", "number": "4"},
+	// 	{"type": "h", "number": "5"},
+	// 	{"type": "h", "number": "6"},
+	// 	{"type": "h", "number": "7"},
+	// 	{"type": "h", "number": "8"},
+	// 	{"type": "h", "number": "9"},
+	// 	{"type": "h", "number": "10"},
+
+	// 	{"type": "s", "number": "1"}, 
+	// 	{"type": "s", "number": "2"},
+	// 	{"type": "s", "number": "3"},
+	// 	{"type": "s", "number": "4"},
+	// 	{"type": "s", "number": "5"},
+	// 	{"type": "s", "number": "6"},
+	// 	{"type": "s", "number": "7"},
+	// 	{"type": "s", "number": "8"},
+	// 	{"type": "s", "number": "9"},
+	// 	{"type": "s", "number": "10"},
+ //        ];
+
+
+    var context = {
+        music: []
+    };
+
+    // var cardClone = $.extend(true, [], cardArray);
+
+    function getRandom() {
+        var random = Math.floor(Math.random() * cardClone.length);
+        var data = cardClone[random];
+        context.music.push(data);
+        cardClone.splice(random, 1);
+        console.log(data);
+    }
+
+	return{
+		"init" : init
+	}
+})();
+
+$(document).ready(function(){
+	CARD.init();
+});
+
+
+
