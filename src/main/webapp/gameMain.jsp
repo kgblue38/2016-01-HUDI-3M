45d0375f68e6587964a2a1ab8b203d67d1c1dd06
Junnie-Jobs <dj0999@naver.com>
2016-04-04 15:25:25 +0900

    game main front ing

--- src/main/webapp/gameMain.jsp
+++ src/main/webapp/gameMain.jsp
@@ -29,40 +29,73 @@
 		UserController uc = new UserController();
 		pageContext.setAttribute("userId1", uc);
 		pageContext.setAttribute("userId2", uc);
+<<<<<<< HEAD
+		
+/* 		User user1 = userDao.findUserById(dj0999);
+		req.getSession().setAttribute("user1", dj0999);
+		
+		User user2 = userDao.findUserById("asd123");
+		req.getSession().setAttribute("user", asd123); */
+		
+
+	%>
+=======
 		pageContext.setAttribute("ur", ur);
 	%> --%>
+>>>>>>> e43d9ff35b4e963231c9b5b6416a615cf32dd7c5
 	
-	<c:choose>
-		<c:when test="${ur.getUserCount() == 1}">
-			<div class="header">
+<%-- 	<c:choose>
+		<c:when test="${ur.getUserCount() == 1}"> --%>
+			<%-- <div class="header">
             <div class="playerA">
                  <div class="playerAAvatar"><img src="image/avartar_in_circle.png"></div>
                  <div class="playerAInformation">Id: ${userId1.getUserId1()} </br> 계급: 루비</div>
+                 <div class="PlayerArubyChips">
+                 	<div class="ruby"></div>
+                 	<div class="ruby"></div>
+                 	<div class="ruby"></div>
+                 	<div class="ruby"></div>
+                 	<div class="ruby"></div>
+                 </div>
                  <div class="playerAScore">53(점)</div>
-            </div>
-		</c:when>
+            </div> --%>
+	<%-- 	</c:when> --%>
 
-		<c:when test="${ur.getUserCount() == 2}">
+		<%-- <c:when test="${ur.getUserCount() == 2}"> --%>
 			<div class="header">
             <div class="playerA">
                  <div class="playerAAvatar"><img src="image/avartar_in_circle.png"></div>
                  <div class="playerAInformation">Id: ${userId1.getUserId1()} </br> 계급: 루비</div>
+                 	 <div class="PlayerArubyChips">
+	                 	<div class="ruby"></div>
+	                 	<div class="ruby"></div>
+	                 	<div class="ruby"></div>
+	                 	<div class="ruby"></div>
+	                 	<div class="ruby"></div>
+               	  </div>
                  <div class="playerAScore">53(점)</div>
             </div>
 
 			<div class="playerB">
                 <div class="playerBAvatar"><img src="image/avartar2_in_circle.png"></div>
                 <div class="playerBInformation">Id: ${userId1.getUserId2()} </br> 계급: 사파이어</div>
+                   <div class="PlayerBrubyChips">
+                    <div class="ruby"></div>
+                    <div class="ruby"></div>
+                    <div class="ruby"></div>
+                    <div class="ruby"></div>
+                    <div class="ruby"></div>
+                  </div>
                 <div class="playerBScore"> 점수
                 </div>
             </div>
-		</c:when>
+<%-- 		</c:when> --%>
 
-		<c:otherwise>
+<%-- 		<c:otherwise>
 			<h1>로그인된 유저가 없습니다.</h1>
 		</c:otherwise>
 
-	</c:choose>
+	</c:choose> --%>
          
        </div>
 
@@ -70,25 +103,43 @@
        <div class="middleArea">
         <div class="gameArea">
             <div class="leftUserArea">
-                <div class="leftUser"> 
-                	<img src="image/leftUser_card.png">
-                </div>
+                <ul class="leftUser"> 
+                  <li class="left_user_card card1"></li>
+                  <li class="left_user_card card2"></li>
+                  <li class="left_user_card card3"></li>
+                  <li class="left_user_card card4"></li>
+                  <li class="left_user_card card5"></li>
+                  <li class="left_user_card card6"></li>
+                  <li class="left_user_card card7"></li>
+                  <li class="left_user_card card8"></li>
+                  <li class="left_user_card card9"></li>
+                  <li class="left_user_card card10"></li>
+                </ul>
             </div>
              <div class="matchArea">
                     <div class="match">
-                    	<div class="leftUser_card">
-                        <!-- <img src="image/card/back.png"> -->
+                    	 <div class="match_left_User_card">
+                         <!-- <img src="image/card/back.png"> -->
                         </div>
-
-                    	<div class="rightUser_card">
+                          <input type="button" value="match" class="match_button"></input>
+                    	<div class="match_right_User_card">
                         <!-- <img src="image/card/back.png"> -->
                     	</div>
                     </div>
             </div> 
             <div class="rightUserArea">
-                <div class="rightUser">
-                	<img src="image/rightUser_card.png">
-                </div>
+                <ul class="rightUser">
+                	<li class="right_user_card card1"></li>
+                  <li class="right_user_card card2"></li>
+                  <li class="right_user_card card3"></li>
+                  <li class="right_user_card card4"></li>
+                  <li class="right_user_card card5"></li>
+                  <li class="right_user_card card6"></li>
+                  <li class="right_user_card card7"></li>
+                  <li class="right_user_card card8"></li>
+                  <li class="right_user_card card9"></li>
+                  <li class="right_user_card card10"></li>                 
+                </ul>
             </div>
             <div class="resultEvent">
 
@@ -113,11 +164,11 @@
                 
        </div>
        <div class="footer">
-            <div class="footerCard">
-            </div>
+           <!--  <div class="footerCard">
+            </div> -->
        </div>
        <script src="/js/gameMain.js"></script>
-       <script src="/js/remodal.js"></script>
+        <script src="/js/remodal.js"></script>
 
     </body>
 </html>
--- src/main/webapp/js/gameMain.js
+++ src/main/webapp/js/gameMain.js
@@ -1,111 +1,152 @@
-var CARD = (function(){
-	var cardTemplate = "<img src='image/card/{{random_card}}h.png'>";
-	var cardTemplate_1 = "<img src='image/card/{{random_card}}s.png'>";
-	var template_up = Handlebars.compile(cardTemplate);
-	var template_up_1 = Handlebars.compile(cardTemplate_1);
-	var resultTemplate = "<p>PlayerA got {{card_NumberA}} Space<br>"+
-						"PlayerB got {{card_NumberB}} Heart</p>";
+var CARD = (function() {
+	// var cardTemplate = "<img src='image/card/{{random_card}}h.png'>";
+	// var cardTemplate_1 = "<img src='image/card/{{random_card}}s.png'>";
+	// var template_up = Handlebars.compile(cardTemplate);
+	// var template_up_1 = Handlebars.compile(cardTemplate_1);
+
+	var resultTemplate = "<p>PlayerA got {{card_NumberA}} Space<br>"
+			+ "PlayerB got {{card_NumberB}} Heart</p>";
 	var resultHeaderTemplate = "<h1>{{winner}}</h1>";
 	var template_up_result = Handlebars.compile(resultTemplate);
 	var template_up_resultHeader = Handlebars.compile(resultHeaderTemplate);
 
-	function getCard(){
+	var card = "<li class='left_user_card card{{cardNum}}'>" + "{{cardNum}}"
+			+ "</li>"
 
-	}
-
-	function revealCard(){
+	function revealCard() {
 		var random_card = Math.floor((Math.random() * 10) + 1);
 		var random_card_1 = Math.floor((Math.random() * 10) + 1);
 		// $(".leftUser_card").append($(template_up({"random_card":random_card})));
-		$(".leftUser_card").delay(1000).queue(function(){
-			$(this).css('background-image', 'url("image/card/'+random_card+'s.png")');
-		});
+		$(".leftUser_card").delay(1000).queue(
+				function() {
+					$(this).css('background-image',
+							'url("image/card/' + random_card + 's.png")');
+				});
 		// $(".rightUser_card").append($(template_up_1({"random_card":random_card_1})));
-		$(".rightUser_card").delay(1000).queue(function(){
-			$(this).css('background-image', 'url("image/card/'+random_card_1+'h.png")');
-		});
-		if(random_card>random_card_1){
-				var result = "Player A wins.";
-				$('#result').remove();
-				$('.playerA').delay(1200).queue(function(){
-					$(this).css('border', '5px solid red');
+		$(".rightUser_card").delay(1000).queue(
+				function() {
+					$(this).css('background-image',
+							'url("image/card/' + random_card_1 + 'h.png")');
 				});
-				$(".remodal p").append($(template_up_result({"card_NumberA":random_card, 
-					"card_NumberB":random_card_1})));
-				$(".remodal h1").append($(template_up_resultHeader({"winner":result})));
-		}else if (random_card == random_card_1){
-				var result = "draw.";
-				$('#result').remove();
-				$(".remodal p").append($(template_up_result({"card_NumberA":random_card, 
-					"card_NumberB":random_card_1})));
-				$(".remodal h1").append($(template_up_resultHeader({"winner":result})));
-		}else{
-				var result = "Player B wins."
-				$('#result').remove();
-				$('.playerB').delay(1200).queue(function(){
-					$(this).css('border', '5px solid red');
-				});
-				$(".remodal p").append($(template_up_result({"card_NumberA":random_card, 
-					"card_NumberB":random_card_1})));
-				$(".remodal h1").append($(template_up_resultHeader({"winner":result})));
+		if (random_card > random_card_1) {
+			var result = "Player A wins.";
+			$('#result').remove();
+			$('.playerA').delay(1200).queue(function() {
+				$(this).css('border', '5px solid red');
+			});
+			$(".remodal p").append($(template_up_result({
+				"card_NumberA" : random_card,
+				"card_NumberB" : random_card_1
+			})));
+			$(".remodal h1").append($(template_up_resultHeader({
+				"winner" : result
+			})));
+		} else if (random_card == random_card_1) {
+			var result = "draw.";
+			$('#result').remove();
+			$(".remodal p").append($(template_up_result({
+				"card_NumberA" : random_card,
+				"card_NumberB" : random_card_1
+			})));
+			$(".remodal h1").append($(template_up_resultHeader({
+				"winner" : result
+			})));
+		} else {
+			var result = "Player B wins."
+			$('#result').remove();
+			$('.playerB').delay(1200).queue(function() {
+				$(this).css('border', '5px solid red');
+			});
+			$(".remodal p").append($(template_up_result({
+				"card_NumberA" : random_card,
+				"card_NumberB" : random_card_1
+			})));
+			$(".remodal h1").append($(template_up_resultHeader({
+				"winner" : result
+			})));
 		}
 	}
-	
-	
 
-	function init(){
-		 $('#result').on('click', revealCard);
-		// $('.gameArea').on('click', '.righttUser_card', getCardRight);
-		
+	function init() {
+
+		get_card_fot_leftuser(shuffle_random_card());
+		match();
+		$('#result').on('click', revealCard);
+		$(".right_user_card").on("dblclick", remove_right);
+		$(".left_user_card").on("dblclick", remove_left);
 	}
 
-	// var cardArray = [
-	// 	{"type": "h", "number": "1"},
-	// 	{"type": "h", "number": "2"},
-	// 	{"type": "h", "number": "3"},
-	// 	{"type": "h", "number": "4"},
-	// 	{"type": "h", "number": "5"},
-	// 	{"type": "h", "number": "6"},
-	// 	{"type": "h", "number": "7"},
-	// 	{"type": "h", "number": "8"},
-	// 	{"type": "h", "number": "9"},
-	// 	{"type": "h", "number": "10"},
+	/* 배열에 랜덤값 얻기 1~10까지의 랜덤 값 넣기 */
+	function shuffle_random_card() {
 
-	// 	{"type": "s", "number": "1"}, 
-	// 	{"type": "s", "number": "2"},
-	// 	{"type": "s", "number": "3"},
-	// 	{"type": "s", "number": "4"},
-	// 	{"type": "s", "number": "5"},
-	// 	{"type": "s", "number": "6"},
-	// 	{"type": "s", "number": "7"},
-	// 	{"type": "s", "number": "8"},
-	// 	{"type": "s", "number": "9"},
-	// 	{"type": "s", "number": "10"},
- //        ];
+		// 결과 담을 배열
+		var cardArray = [];
+		var temp; // 난수 임시로 담을 변수
+		var CheckFlag = 0; // 중복체크 Flag (0,1)
 
+		for (var i = 0; i < 10; i++) {
+			temp = Math.floor(Math.random() * 10) + 1; // 난수 생성 (+1 은 0 제외)
+			for (var j = 0; j < i; j++) { // 기존 배열 값들 0 부터 i 번째(현재) 전까지
+				if (cardArray[j] == temp) { // 중복체크 _ 같은것이 있다면,
+					i = i - 1; // 현재 배열위치 i 에서 한칸 앞 위치를 가져간다. *( i++ 가 되니 미리
+								// i-1해준다.) 결국은 i값이 증가되지 않는것.
+					CheckFlag = 1; // 배열에 값을 넣지 않기 위해 중복체크 플래그 값 설정해주고
+				}
+			}
 
-    var context = {
-        music: []
-    };
+			if (CheckFlag == 0) { // 중복된 값이 아니면 난수를 배열 i 번째 넣어주고.
+				cardArray[i] = temp;
+			} else {
+				CheckFlag = 0; // 중복된 값이면 i -1 번째 그대로 간다.
+			}
+		}
 
-    // var cardClone = $.extend(true, [], cardArray);
+		return cardArray;
+	}
 
-    function getRandom() {
-        var random = Math.floor(Math.random() * cardClone.length);
-        var data = cardClone[random];
-        context.music.push(data);
-        cardClone.splice(random, 1);
-        console.log(data);
-    }
+	function get_card_fot_leftuser(cardArray) {
 
-	return{
+	}
+
+	function remove_left(e) {
+
+		var left_user_card = $(".match_left_User_card");
+
+		if (!left_user_card.hasClass('put_card')) {
+			$(e.target).remove();
+			left_user_card.css('display', 'block');
+			left_user_card.addClass('put_card');
+		} else
+			alert("낙장불입!");
+
+	}
+
+	function remove_right(e) {
+
+		var right_user_card = $(".match_right_User_card");
+
+		if (!right_user_card.hasClass('put_card')) {
+			$(e.target).remove();
+			right_user_card.css('display', 'block');
+			right_user_card.addClass('put_card');
+		} else
+			alert("낙장불입!");
+	}
+
+	return {
 		"init" : init
+	}
+
+	function match() {
+
+		var right_user_card = $(".match_right_User_card");
+		var left_user_card = $(".match_left_User_card");
+
+		if (right_user_card.hasClass('put_card') && left_user_card('put_card'))
+			$(".match_button").css("display", "block");
 	}
 })();
 
-$(document).ready(function(){
+$(document).ready(function() {
 	CARD.init();
 });
-
-
-
